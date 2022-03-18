ca1a15426f40f468fc0359d1ba8298e174856ef6
tsenturion <sony1920145@gmail.com>
2022-03-18 22:55:19 +0500

    homework_4

---
+++ homeworks/homework_4/src/main/scala/Cell.scala
@@ -0,0 +1,23 @@
+trait Cell
+
+class EmptyCell() extends Cell {
+  override def toString: String = "empty"
+}
+
+class NumberCell(number: Int) extends Cell {
+  override def toString: String = number.toString
+}
+
+class StringCell(string: String) extends Cell {
+  override def toString: String = string
+}
+
+class ReferenceCell(val ix: Int, val iy: Int, table: Table) extends Cell {
+  override def toString: String = {
+    table.getCell(ix, iy) match {
+      case Some(cell: ReferenceCell) => if (table.getCell(cell.ix, cell.iy).contains(this)) "cyclic" else cell.toString
+      case Some(cell: Cell) => cell.toString
+      case None => "outOfRange"
+    }
+  }
+}
